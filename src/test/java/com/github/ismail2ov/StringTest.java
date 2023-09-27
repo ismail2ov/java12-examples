3200dc0b9cb90a1cef44df3125bb5dd483c2a54c
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-09-27 20:43:10 +0200

    String transform example

--- src/test/java/com/github/ismail2ov/StringTest.java
+++ src/test/java/com/github/ismail2ov/StringTest.java
@@ -2,6 +2,7 @@ package com.github.ismail2ov;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 class StringTest {
@@ -19,4 +20,14 @@ class StringTest {
         assertThat(actual).isEqualTo(expected);
     }
 
+    @Test
+    void testStringTransform() {
+        String uppercase = "abcde".transform(String::toUpperCase);
+        Integer i = "12345".transform(Integer::valueOf);
+        BigDecimal big = "1234567891011121314151617181920".transform(BigDecimal::new);
+
+        assertThat(uppercase).isEqualTo("abcde".toUpperCase());
+        assertThat(i).isEqualTo(Integer.valueOf("12345"));
+        assertThat(big).isEqualTo(new BigDecimal("1234567891011121314151617181920"));
+    }
 }
